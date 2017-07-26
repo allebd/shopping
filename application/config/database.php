f4bae9cb60e8e2a8af1f0f8c330ea9b896837b2d
Bella Oyedele <delebella@yahoo.com>
2017-07-26 20:16:10 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'IBUKUN';
-$db['default']['database'] = 'gracefoods';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
